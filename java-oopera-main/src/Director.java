a8825641514c4fb2792ea0a37f51cec23b5ea0c7
petya <Chastushk@yandex.ru>
2026-04-20 11:09:29 +0300

    Delete java-oopera-main directory

--- java-oopera-main/src/Director.java
+++
@@ -1,8 +0,0 @@
-public class Director extends Person {
-    private int numberOfShows;
-
-    public Director(String name, String surname, GenderOfPeople gender, int numberOfShows) {
-        super(name, surname, gender); // вызов конструктора родителя
-        this.numberOfShows = numberOfShows;
-    }
-}
