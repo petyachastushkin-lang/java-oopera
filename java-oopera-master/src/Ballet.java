758ce05e93f0952e7c22414f9d3faeadba1ae6d5
petya <Chastushk@yandex.ru>
2026-04-20 11:01:29 +0300

    Delete java-oopera-master directory

--- java-oopera-master/src/Ballet.java
+++
@@ -1,9 +0,0 @@
-public class Ballet extends MusicalShow {
-    private Person Choreographer;
-
-    public Ballet(String title, int duration, Person director,
-                  Person musicAuthor, String librettoText, Person choreographer) {
-        super(title, duration, director, musicAuthor, librettoText);  // Вызываем родительский конструктор
-        this.Choreographer = choreographer;  // Задаём своё уникальное поле
-    }
-}
